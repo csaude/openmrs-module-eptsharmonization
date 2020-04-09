993eb4753846064cae54eff43d6270db56d6c099
guimino <minoneves@gmail.com>
2020-04-09 07:11:20 +0200

    Initial commit

---
+++ api/src/main/java/org/openmrs/module/eptsharmonization/EptsHarmonizationActivator.java
@@ -0,0 +1,75 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License. You may obtain a copy of the
+ * License at http://license.openmrs.org
+ *
+ * <p>Software distributed under the License is distributed on an "AS IS" basis, WITHOUT WARRANTY OF
+ * ANY KIND, either express or implied. See the License for the specific language governing rights
+ * and limitations under the License.
+ *
+ * <p>Copyright (C) OpenMRS, LLC. All Rights Reserved.
+ */
+package org.openmrs.module.eptsharmonization;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.openmrs.api.context.Context;
+import org.openmrs.module.BaseModuleActivator;
+import org.openmrs.module.dataexchange.DataImporter;
+
+/**
+ * This class contains the logic that is run every time this module is either started or shutdown
+ */
+public class EptsHarmonizationActivator extends BaseModuleActivator {
+
+  private Log log = LogFactory.getLog(this.getClass());
+
+  @Override
+  public void contextRefreshed() {
+    log.debug("EPTS  Harmonization Module refreshed");
+  }
+
+  @Override
+  public void willRefreshContext() {
+    log.debug("Refreshing Epts Harmonization Module");
+  }
+
+  @Override
+  public void willStart() {
+    log.debug("Starting Epts Harmonization Module");
+  }
+
+  @Override
+  public void willStop() {
+    log.debug("Stopping Epts Harmonization Module");
+  }
+
+  public void started() {
+    log.info("Starting Epts Harmonization Module");
+    this.installMetaData();
+  }
+
+  private void installMetaData() {
+    DataImporter dataImporter = Context.getRegisteredComponents(DataImporter.class).get(0);
+
+    log.info("Importing _encounter_type Metadata");
+    dataImporter.importData("encounter-types.xml");
+    log.info(" _encounter_type Metadata imported");
+  }
+
+  @Override
+  public void stopped() {
+    log.info("Shutting down Epts Harmonization Module");
+    this.deletePreviousHarmonizationLoadedDDLAndLiquibase();
+  }
+
+  private void deletePreviousHarmonizationLoadedDDLAndLiquibase() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("DROP TABLE IF EXISTS `_encounter_type`");
+    Context.getAdministrationService().executeSQL(sb.toString(), false);
+
+    sb = new StringBuilder();
+    sb.append("delete from liquibasechangelog where ID ='20200402-1806'");
+    Context.getAdministrationService().executeSQL(sb.toString(), false);
+  }
+}
